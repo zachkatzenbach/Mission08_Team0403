5526db98e458ce9c7ae6b942aa1215bccfc3d2e1
haydenwhalen <153124475+haydenwhalen@users.noreply.github.com>
2025-02-26 16:32:57 -0700

    h

--- Mission08_Team0403/Models/Task.cs
+++ Mission08_Team0403/Models/Task.cs
@@ -1,6 +1,25 @@
-namespace Mission08_Team0403.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Task
+namespace Mission08_Team0403.Models
 {
-    
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }  // 1, 2, 3, or 4
+
+        [Required]
+        public int CategoryId { get; set; }  // Foreign Key
+        public Category Category { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool Completed { get; set; } = false; // Default: Not completed
+    }
 }
