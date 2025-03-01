457fd401b06a92a11c653d559d705066baf5d7ac
Zach Katzenbach <zachkatzenbach@gmail.com>
2025-02-28 20:59:40 -0700

    Add and edit work

--- Mission08_Team0403/Models/ITaskRepository.cs
+++ Mission08_Team0403/Models/ITaskRepository.cs
@@ -10,5 +10,7 @@ namespace Mission08_Team0403.Models
         public IEnumerable<Task> GetTasksWithDetails();
 
         public void AddTask(Task task);
+
+        public void UpdateTask(Task task);
     }
 }
